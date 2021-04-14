82300598467c310cc1db96c627db1fd73127e372
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2021-04-14 20:18:19 +0900

    render

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
 const Didact = {
   createElement,
+  render,
 };
 
 /** @jsxRuntime classic */
 /** @jsx Didact.createElement */
 const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b></b>
+  <div>
+    <h1>Hello World</h1>
+    <h2>from Didact</h2>
   </div>
 );
 
 console.log(element);
-const container = document.getElementById("root");
+const container = document.getElementById("root") as HTMLElement;
+Didact.render(element, container);
 
 function createElement(
   type: string,
@@ -42,6 +44,25 @@ function createElement(
       }),
     },
   };
+}
+
+function render(element: JSX.Element, container: HTMLElement) {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = (key: string) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name: string) => {
+      dom[name] = element.props[name];
+    });
+
+  element.props.children.forEach((child: any) => {
+    render(child, dom);
+  });
+  container.appendChild(dom);
 }
 
 export {};
